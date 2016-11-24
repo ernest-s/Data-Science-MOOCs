144b2bd09252ad94e121a304f8469514ec4dc9a7
Ernest S <ernest.kirubakaran@gmail.com>
2016-11-24 16:22:22 +0530

    added matrix addition function

---
+++ Linear_Algebra-UT.5.04x/Week03/Matrix_addition_unb.m
@@ -0,0 +1,41 @@
+
+% Copyright 2016 The University of Texas at Austin
+%
+% For licensing information see
+%                http://www.cs.utexas.edu/users/flame/license.html 
+%                                                                                 
+% Programmed by: Ernest Kirubakaran Selvaraj
+%                ernest.kirubakaran@gmail.com
+
+function [ A_out ] = Scale_matrix_unb( B, A )
+
+  [ BL, BR ] = FLA_Part_1x2( B, ...
+                               0, 'FLA_LEFT' );
+
+  [ AL, AR ] = FLA_Part_1x2( A, ...
+                               0, 'FLA_LEFT' );
+
+  while ( size( BL, 2 ) < size( B, 2 ) )
+
+    [ B0, b1, B2 ]= FLA_Repart_1x2_to_1x3( BL, BR, ...
+                                         1, 'FLA_RIGHT' );
+
+    [ A0, a1, A2 ]= FLA_Repart_1x2_to_1x3( AL, AR, ...
+                                         1, 'FLA_RIGHT' );
+
+    %------------------------------------------------------------%
+
+    a1 = a1 + b1;
+    %------------------------------------------------------------%
+
+    [ BL, BR ] = FLA_Cont_with_1x3_to_1x2( B0, b1, B2, ...
+                                           'FLA_LEFT' );
+
+    [ AL, AR ] = FLA_Cont_with_1x3_to_1x2( A0, a1, A2, ...
+                                           'FLA_LEFT' );
+
+  end
+
+  A_out = [ AL, AR ];
+
+return
